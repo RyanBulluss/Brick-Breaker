75199cfb9672aef472ffbf433a0161ba53d0bf8f
RyanBulluss <rbulluss2000@gmail.com>
2024-02-17 12:17:43 +0000

    Create collision detection and ball moving mechanics. Also add a start button

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,14 +1,51 @@
 import { useState, useEffect } from "react";
 import "./constants";
-import { board, paddle, ball } from "./constants";
+import { board, paddle, ball, findX, rng } from "./constants";
 
 export default function Game() {
+  const [playing, setPlaying] = useState(true);
   const [paddleX, setPaddleX] = useState(0);
-  const [ballX, setBallX] = useState(0);
+  const [ballX, setBallX] = useState(board.width / 2 - ball.width / 2);
   const [ballY, setBallY] = useState(0);
-  const [ballSpeed, setBallSpeed] = useState({ x: 2, y: 2 });
+  const [ballSpeed, setBallSpeed] = useState({ x: rng(20) - 10, y: 6 });
 
+  useEffect(() => {
+    const gameLoop = setInterval(() => {
+      if (!playing) return;
+      const newX = ballX + ballSpeed.x;
+      const newY = ballY + ballSpeed.y;
+      setBallX(newX);
+      setBallY(newY);
 
+      if (newY + 10 >= board.height) {
+        setPlaying(false);
+      }
+
+      if (newX < 0 || newX > board.width - ball.width) {
+        setBallSpeed((prevSpeed) => ({ ...prevSpeed, x: -prevSpeed.x }));
+      }
+      if (newY < 0 || newY > board.height - ball.height) {
+        setBallSpeed((prevSpeed) => ({ ...prevSpeed, y: -prevSpeed.y }));
+      }
+      if (
+        newY > board.height - paddle.height &&
+        newX >= paddleX &&
+        newX <= paddleX + paddle.width
+      ) {
+        let newXSpeed = findX(newX, paddleX);
+        setBallSpeed((prevSpeed) => ({ x: newXSpeed, y: -prevSpeed.y }));
+      }
+    }, 16);
+
+    return () => clearInterval(gameLoop); // Cleanup function to clear interval when component unmounts
+  }, [ballX, ballY, ballSpeed]);
+
+  function startGame() {
+    setBallX(board.width / 2 - ball.width / 2);
+    setBallY(0);
+    setPlaying(true);
+    setBallSpeed({ x: rng(20) - 10, y: 6 });
+  }
 
   const handleMouseMove = (e: any) => {
     const mouseX = e.clientX;
@@ -32,13 +69,16 @@ export default function Game() {
         }}
         className="board"
       >
-        <div style={{
+        <div
+          style={{
             height: ball.height,
             width: ball.width,
             backgroundColor: ball.color,
             top: ballY,
             left: ballX,
-        }}></div>
+          }}
+          className="rounded-full"
+        ></div>
         <div
           style={{
             height: paddle.height,
@@ -49,6 +89,7 @@ export default function Game() {
           }}
         ></div>
       </div>
+      <button onClick={() => startGame()} className="p-4 bg-red-600 hover:bg-red-700 text-white rounded-full m-4">Start Game</button>
     </div>
   );
 }
--- src/constants.ts
+++ src/constants.ts
@@ -5,15 +5,32 @@ const board = {
 };
 
 const ball = {
-    height: 10,
-    width: 10,
+    height: 15,
+    width: 15,
     color:"black"
 }
 
 const paddle = {
   height: 30,
-  width: 80,
+  width: 100,
   color: "black",
 };
 
-export { board, paddle, ball };
+function findX(ballX: number, paddleX: number) {
+    const leftOfBall = ballX - paddleX;
+    const w = paddle.width
+    if (leftOfBall < w/8) return -10;
+    if (leftOfBall < w/4) return -6;
+    if (leftOfBall < w/3) return -3;
+    if (leftOfBall < w/2) return -1;
+    if (leftOfBall < w/1.8) return 1;
+    if (leftOfBall < w/1.6) return 3;
+    if (leftOfBall < w/1.3) return 6;
+    return 10
+}
+
+function rng(n: number) {
+    return Math.floor(Math.random() * n);
+}
+
+export { board, paddle, ball, findX, rng };
